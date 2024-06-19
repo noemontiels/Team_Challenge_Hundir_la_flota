b1f13f65d444605439ece4d11721c42154349182
noemontiels <165649802+noemontiels@users.noreply.github.com>
2024-06-19 19:16:13 +0200

    Update variables.py

--- variables.py
+++ variables.py
@@ -21,13 +21,10 @@ Variables: Esto iría en main.py porque van cambiando durante el juego
 
 # ID del jugador
 PLAYER_ID = input('Introduzca su nombre: ') # ESTO NO SÉ SI SERÍA ASÍ
-
 # ID máquina
 PC_ID = 'player_pc'
-
 # Dimensiones del tablero
 BOARD_DIMENSION = 10
-
 # Barcos
 BOATS = {                             
     'A': ['A1'],
@@ -35,6 +32,5 @@ BOATS = {
     'C': ['C1', 'C2', 'C3'],
     'D': ['D1', 'D2', 'D3', 'D4']
 }
-
 # Tablero vacío
 EMPTY_BOARD = np.full((BOARD_DIMENSION, BOARD_DIMENSION), ' ')
