2933bd21eeb755f807b3576954f75c4d7ce81be3
María DM <mariad1996@gmail.com>
2024-06-19 14:07:37 +0200

    added: funciones_clase.ipynb

--- funciones.py
+++ funciones.py
@@ -1,1 +1,18 @@
 import numpy as np
+
+'''
+1. Inicio juego
+2. Generar jugador
+3. Colocar barco
+4. Determinar tipo de jugador
+5. Pedir coordenadas si jugador humano
+6. Marcar tablero
+7. Mostrar tablero actualizado después del disparo (los 2 tableros)
+8. Determinar si se ha tocado / hundido un barco
+9. Determinar turno: si ha tocado continua el mismo jugador, si no, pasa al otro
+10. Fin del juego si todos los barcos de un jugador están hundidos
+11. Salida del juego voluntaria
+12. Verificar que coordenadas están en el tablero
+13. Verifcar que pc no dispara 2 veces al mismo sitio
+14. Mostrar tablero (reveal_ship): cada jugador tiene 2, uno donde tiene sus barcos colocados y otro donde dispara. Solo se muestra el tablero de los disparos
+'''
--- variables.py
+++ variables.py
@@ -1,1 +1,8 @@
 import numpy as np
+
+'''
+- Registro de coordenadas de disparo (pc)
+- Id jugador humano
+- Id pc
+- Tableros (jugador + pc)
+'''
