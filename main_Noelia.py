7c52dcb0a8c76c10f753fca3f720134ac858a2c1
noemontiels <165649802+noemontiels@users.noreply.github.com>
2024-06-20 13:29:48 +0200

    Update main_Noelia.py

--- main_Noelia.py
+++ main_Noelia.py
@@ -5,23 +5,45 @@ import funciones
 
 # Cuerpo principal del juego
 def main():
+    
     # Inicio del juego
     funciones.welcome()
     variables.PLAYER_ID = funciones.player_name()
 
     # Inicializar los tableros
-    PLAYER_BOARD = clases.Board(Player_ID)
+    PLAYER_BOARD = clases.Board(player_id = variables.PLAYER_ID)
+    PC_BOARD = clases.Board(player_id = 'PC')
 
-# Variables de la partida
-pc_shots = []
+    # Variables de la partida
+    player_lives = 20
+    pc_lives = 20
+    pc_shots = []
 
-
-
-
-# Si PLAYER_ID es distinto de 'exit', se inicia el juego
-while variables.PLAYER_ID != 'exit':
+    # Si PLAYER_ID es distinto de 'exit', se inicia el juego
+    while variables.PLAYER_ID != 'exit':
     print('Dentro de while')
     break
+    
+    # Bucle principal del juego
+    player_turn = True
+    while True:
+        if player_turn:
+            print('Tu tablero')
+            funciones.print_board(PLAYER_BOARD, show_shots = True)
+            print('Tablero del oponente')
+            funciones.print_board(PC_BOARD, show_shots = True)
+
+            action, x, y = funciones.get_shot_coordinates()
+            if action == 'exit':
+                print('Saliendo del juego...')
+                break
+            elif action == 'print'
+            
+
+
+
+
+
 
 
 print('\n¡Hasta pronto!')
